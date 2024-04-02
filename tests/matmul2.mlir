39b6f00abb7ae7821418ee1c191e503d9dc18b11
EmilySillars <emsillars@gmail.com>
2024-04-02 16:36:01 +0100

    complete python object for matmul example

---
+++ tests/matmul2.mlir
@@ -0,0 +1,9 @@
+"builtin.module"() ({
+  %0:3 = "test.op"() : () -> (memref<5x7xf32>, memref<7x6xf32>, memref<5x6xf32>)
+  "linalg.generic"(%0#0, %0#1, %0#2) <{indexing_maps = [affine_map<(d0, d1, d2) -> (d0, d2)>, affine_map<(d0, d1, d2) -> (d2, d1)>, affine_map<(d0, d1, d2) -> (d0, d1)>], iterator_types = [#linalg.iterator_type<parallel>, #linalg.iterator_type<parallel>, #linalg.iterator_type<reduction>], operandSegmentSizes = array<i32: 2, 1>}> ({
+  ^bb0(%arg0: f32, %arg1: f32, %arg2: f32):
+    %1 = "arith.mulf"(%arg0, %arg1) <{fastmath = #arith.fastmath<none>}> : (f32, f32) -> f32
+    %2 = "arith.addf"(%arg2, %1) <{fastmath = #arith.fastmath<none>}> : (f32, f32) -> f32
+    "linalg.yield"(%2) : (f32) -> ()
+  }) : (memref<5x7xf32>, memref<7x6xf32>, memref<5x6xf32>) -> ()
+}) : () -> ()
--- transforms/linalg_to_stream.py
+++ transforms/linalg_to_stream.py
@@ -19,14 +19,15 @@ from util.kernel_type import KernelType
 class LinalgToStreamTranslator(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(self, generic_op: Generic, rewriter: PatternRewriter):
-        print("found a linalg generic op!")
 
         kernel_type = KernelType.get_kernel(generic_op)
 
         if not kernel_type:
             return
-
-        print(f"detected a kernel type of {kernel_type}")
+        
+        # if kernel_type != KernelType.MUL:
+        #     print(f"kernel type of {kernel_type} not yet supported")
+        #     return
 
         # zigzag -> 3 operands: 2 inputs, 1 output
         assert len(generic_op.outputs) == 1
@@ -38,6 +39,7 @@ class LinalgToStreamTranslator(RewritePattern):
 
         zigzag_description = dict()
 
+        # for now, set operator to default type
         zigzag_description["operator_type"] = "default"
 
         # construct equation
@@ -46,31 +48,33 @@ class LinalgToStreamTranslator(RewritePattern):
             map = generic_op.indexing_maps.data[-1].data.results[i]
             assert isinstance(map, AffineDimExpr)
             output_access += f"[{str(map)}]"
-
-        input_a_access = "A"
+       
+        input_i_access = "I"
         for i in range(len(generic_op.indexing_maps.data[0].data.results)):
             map = generic_op.indexing_maps.data[0].data.results[i]
             assert isinstance(map, AffineDimExpr)
-            input_a_access += f"[{str(map)}]"
+            input_i_access += f"[{str(map)}]"
 
-        input_b_access = "B"
+        input_w_access = "W"
         for i in range(len(generic_op.indexing_maps.data[1].data.results)):
             map = generic_op.indexing_maps.data[1].data.results[i]
             assert isinstance(map, AffineDimExpr)
-            input_b_access += f"[{str(map)}]"
+            input_w_access += f"[{str(map)}]"
 
         if kernel_type == KernelType.MUL:
             zigzag_description[
                 "equation"
-            ] = f"{output_access} = {input_a_access} * {input_b_access}"
+            ] = f"{output_access} = {input_i_access} * {input_w_access}"
 
         elif kernel_type in (KernelType.MAC, KernelType.QMAC):
             zigzag_description[
                 "equation"
-            ] = f"{output_access} += {input_a_access} * {input_b_access}"
+            ] = f"{output_access} += {input_i_access} * {input_w_access}"
 
+        # extract dimension_relations (for now, leave empty)
         zigzag_description["dimension_relations"] = []
 
+        # extract loop bounds
         results = []
         results.extend(generic_op.indexing_maps.data[0].data.results)
         results.extend(generic_op.indexing_maps.data[1].data.results)
@@ -87,8 +91,7 @@ class LinalgToStreamTranslator(RewritePattern):
         for i, bound in enumerate(iteration_bounds):
             zigzag_description["loop_dim_size"][f"d{i}"] = bound
 
-    
-      
+        # extract operand precision
         widths = []
         for op in generic_op.operands :
             assert isinstance(op.type, memref.MemRefType)           
@@ -103,8 +106,22 @@ class LinalgToStreamTranslator(RewritePattern):
         zigzag_description["operand_precision"]["O_final"] = widths[-1]
         zigzag_description["operand_precision"]["W"] = widths[0]
         zigzag_description["operand_precision"]["I"] = widths[1]        
-       
-        print(zigzag_description)
+  
+        # operand source (use default of no source for now)
+        zigzag_description["operand_source"] = dict()
+        zigzag_description["operand_source"]["W"] = []
+        zigzag_description["operand_source"]["I"] = []
+
+        # constant operands (use default of all constant operands for now)
+        zigzag_description["constant_operands"] = dict()
+        zigzag_description["constant_operands"] = ["I", "W"]
+
+        # padding (use default of 0 padding for now)
+        # affects last two indices of input I
+        zigzag_description["padding"] = dict()
+        zigzag_description["padding"][str(generic_op.indexing_maps.data[0].data.results[0]).upper()] = (0,0)
+        zigzag_description["padding"][str(generic_op.indexing_maps.data[0].data.results[1]).upper()] = (0,0)
+        print(f"workload = {zigzag_description}")
         print("")
 
         pass
