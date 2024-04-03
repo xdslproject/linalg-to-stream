e8b62e7bcc364c03c3417352dcef59e7d9205533
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-04-03 16:47:22 +0100

    add input parsing

--- transforms/linalg_to_stream.py
+++ transforms/linalg_to_stream.py
@@ -10,6 +10,7 @@ from xdsl.pattern_rewriter import (
     op_type_rewrite_pattern,
 )
 from xdsl.dialects.builtin import (
+    ShapedType,
     IntegerType,
     IntAttr
 )
@@ -27,14 +28,25 @@ class LinalgToStreamTranslator(RewritePattern):
 
         if not kernel_type:
             return
-
-        # zigzag -> 3 operands: 2 inputs, 1 output
-        assert len(generic_op.outputs) == 1
-        generic_op.outputs[0]
-
-        assert len(generic_op.inputs) == 2
-        generic_op.inputs[0]
-        generic_op.inputs[1]
+        
+        # make some assertions correct outputs of the linalg generic
+        # linalg should have one output, and it should be a shaped type
+        if len(generic_op.outputs) != 1:
+            return
+        if not isinstance(generic_op.outputs[0].type, ShapedType):
+            return
+        
+        # extract output op and relevant indexing maps
+        output_op = generic_op.outputs[0]
+        output_map = generic_op.indexing_maps.data[-1].data
+        
+        # make some assertions on correct inputs of the linalg generic
+        shaped_inputs = [(op, map.data) for (op, map) in zip(generic_op.inputs, generic_op.indexing_maps.data[:-1]) if isinstance(op.type, ShapedType)]
+        if len(shaped_inputs) != 2:
+            return
+        
+        operands = [shaped_inputs[0][0], shaped_inputs[1][0], output_op]
+        indexing_maps = [shaped_inputs[0][1], shaped_inputs[1][1], output_map]
 
         zigzag_description = dict()
 
@@ -43,20 +55,20 @@ class LinalgToStreamTranslator(RewritePattern):
 
         # construct equation
         output_access = "O"
-        for i in range(len(generic_op.indexing_maps.data[-1].data.results)):
-            map = generic_op.indexing_maps.data[-1].data.results[i]
+        for i in range(len(indexing_maps[-1].results)):
+            map = indexing_maps[-1].results[i]
             assert isinstance(map, AffineDimExpr)
             output_access += f"[{str(map)}]"
        
         input_i_access = "I"
-        for i in range(len(generic_op.indexing_maps.data[0].data.results)):
-            map = generic_op.indexing_maps.data[0].data.results[i]
+        for i in range(len(indexing_maps[0].results)):
+            map = indexing_maps[0].results[i]
             assert isinstance(map, AffineDimExpr)
             input_i_access += f"[{str(map)}]"
 
         input_w_access = "W"
-        for i in range(len(generic_op.indexing_maps.data[1].data.results)):
-            map = generic_op.indexing_maps.data[1].data.results[i]
+        for i in range(len(indexing_maps[1].results)):
+            map = indexing_maps[1].results[i]
             assert isinstance(map, AffineDimExpr)
             input_w_access += f"[{str(map)}]"
 
@@ -77,14 +89,14 @@ class LinalgToStreamTranslator(RewritePattern):
         # extract loop bounds by evaluating the inverse affine map
         # with the memref shapes as input
         results = []
-        results.extend(generic_op.indexing_maps.data[0].data.results)
-        results.extend(generic_op.indexing_maps.data[1].data.results)
-        results.extend(generic_op.indexing_maps.data[2].data.results)
+        results.extend(indexing_maps[0].results)
+        results.extend(indexing_maps[1].results)
+        results.extend(indexing_maps[2].results)
 
         combined_affine_map = AffineMap(3, 0, results)
         inverse_map = combined_affine_map.inverse_permutation()
 
-        memref_shapes = [shape.data for op in generic_op.operands for shape in op.type.shape.data]
+        memref_shapes = [shape.data for op in operands for shape in op.type.shape.data]
         iteration_bounds = inverse_map.eval(memref_shapes, [])
 
         zigzag_description["loop_dim_size"] = dict()
@@ -94,7 +106,7 @@ class LinalgToStreamTranslator(RewritePattern):
 
         # extract operand precision
         widths = []
-        for op in generic_op.operands :
+        for op in operands:
             assert isinstance(op.type, memref.MemRefType)           
             element_type = op.type.get_element_type()
             if(isinstance(element_type, IntegerType)):
@@ -120,8 +132,8 @@ class LinalgToStreamTranslator(RewritePattern):
         # padding (use default of 0 padding for now)
         # affects last two indices of input I
         zigzag_description["padding"] = dict()
-        zigzag_description["padding"][str(generic_op.indexing_maps.data[0].data.results[0]).upper()] = (0,0)
-        zigzag_description["padding"][str(generic_op.indexing_maps.data[0].data.results[1]).upper()] = (0,0)
+        zigzag_description["padding"][str(indexing_maps[0].results[0]).upper()] = (0,0)
+        zigzag_description["padding"][str(indexing_maps[0].results[1]).upper()] = (0,0)
         workload= dict()
         workload[0] = zigzag_description
 
